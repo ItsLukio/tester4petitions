d6dfd1161653914325b655fe913385344d4d0041
fahyl <l.fahy12@universityofgalway.ie>
2024-11-12 17:15:03 +0000

    Initial commit

---
+++ tester4petitions/src/main/java/com/example/tester4petitions/Tester4petitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.tester4petitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tester4petitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tester4petitionsApplication.class, args);
+	}
+
+}
---
+++ tester4petitions/src/test/java/com/example/tester4petitions/Tester4petitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tester4petitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tester4petitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
